14a7284b6739635adb521b6868a6d98d0760e428
wangchong <chong.wang@iqunxing.com>
2018-01-18 09:23:26 +0800

    样式格式化

--- src/components/index/datastrcture/LinkedList.vue
+++ src/components/index/datastrcture/LinkedList.vue
@@ -3,109 +3,109 @@
     <h3>链表构造函数如下</h3>
     <pre>
       function LinkedList () {
-      this.first = null
-      this.last = null
-      this.size = 0
+        this.first = null
+        this.last = null
+        this.size = 0
 
-      function Node (ele) {
-        this.next = null
-        this.prev = null
-        this.ele = ele
-      }
+        function Node (ele) {
+          this.next = null
+          this.prev = null
+          this.ele = ele
+        }
 
-      this.addFirst = function (ele) {
-        var node = new Node(ele)
-        node.next = this.first
-        if (this.size > 0) {
-          this.first.prev = node
-        }
-        this.first = node
-        if (this.size === 0) {
-          this.last = this.first
-        } else {
+        this.addFirst = function (ele) {
+          var node = new Node(ele)
+          node.next = this.first
+          if (this.size > 0) {
+            this.first.prev = node
+          }
+          this.first = node
+          if (this.size === 0) {
+            this.last = this.first
+          } else {
 
+          }
+          this.size++
         }
-        this.size++
-      }
 
-      this.addLast = function (ele) {
-        var node = new Node(ele)
-        node.prev = this.last
-        if (this.size > 0) {
-          this.last.next = node
+        this.addLast = function (ele) {
+          var node = new Node(ele)
+          node.prev = this.last
+          if (this.size > 0) {
+            this.last.next = node
+          }
+          this.last = node
+          if (this.size === 0) {
+            this.first = this.last
+          }
+          this.size++
         }
-        this.last = node
-        if (this.size === 0) {
-          this.first = this.last
-        }
-        this.size++
-      }
 
-      this.removeFirst = function () {
-        if (this.size === 0) {
-          return false
+        this.removeFirst = function () {
+          if (this.size === 0) {
+            return false
+          }
+          this.first = this.first.next
+          if (this.first) {
+            this.first.prev = null
+          } else {
+            this.last = null
+          }
+          this.size--
+          return true
         }
-        this.first = this.first.next
-        if (this.first) {
-          this.first.prev = null
-        } else {
-          this.last = null
-        }
-        this.size--
-        return true
-      }
 
-      this.removeLast = function () {
-        if (this.size === 0) {
-          return false
+        this.removeLast = function () {
+          if (this.size === 0) {
+            return false
+          }
+          this.last = this.last.prev
+          if (this.last) {
+            this.last.next = null
+          } else {
+            this.first = null
+          }
+          this.size--
+          return true
         }
-        this.last = this.last.prev
-        if (this.last) {
-          this.last.next = null
-        } else {
-          this.first = null
-        }
-        this.size--
-        return true
-      }
 
-      this.removeIndex = function (index) {
-        if (index >= this.size || index <= 0) {
-          return false
-        }
-        switch (index) {
-          case 1:
-            this.removeFirst()
-            break
-          case (this.size - 1):
-            this.removeLast()
-            break
-          default:
-            let count = 1
-            let currentNode = this.first
-            while (count !== index) {
-              count++
-              currentNode = currentNode.next
-              if (count === index) {
-                currentNode.prev.next = currentNode.next
-                currentNode.next.prev = currentNode.prev
-                currentNode = null
+        this.removeIndex = function (index) {
+          if (index >= this.size || index <= 0) {
+            return false
+          }
+          switch (index) {
+            case 1:
+              this.removeFirst()
+              break
+            case (this.size - 1):
+              this.removeLast()
+              break
+            default:
+              let count = 1
+              let currentNode = this.first
+              while (count !== index) {
+                count++
+                currentNode = currentNode.next
+                if (count === index) {
+                  currentNode.prev.next = currentNode.next
+                  currentNode.next.prev = currentNode.prev
+                  currentNode = null
+                }
               }
-            }
-            break
+              break
+          }
         }
-      }
 
-      this.toString = function () {
-        var currentNode = this.first
-        var arr = []
-        while (currentNode) {
-          arr.push(currentNode.ele)
-          currentNode = currentNode.next
+        this.toString = function () {
+          var currentNode = this.first
+          var arr = []
+          while (currentNode) {
+            arr.push(currentNode.ele)
+            currentNode = currentNode.next
+          }
+          return '[' + arr.join(',') + ']'
         }
-        return '[' + arr.join(',') + ']'
       }
-    }
     </pre>
     <h3>方法如下</h3>
     <pre>
