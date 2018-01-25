e3d7c27de569a470e1d39f13b5b5fd56213434c6
wangchong <chong.wang@iqunxing.com>
2018-01-25 10:00:23 +0800

    修改linkedlist删除时的返回，添加二叉树

--- src/assets/js/CommonUtil.js
+++ src/assets/js/CommonUtil.js
@@ -1,4 +1,4 @@
-function LinkedList () {
+export const LinkedList = function () {
   this.first = null
   this.last = null
   this.size = 0
@@ -41,6 +41,7 @@ function LinkedList () {
     if (this.size === 0) {
       return false
     }
+    const first = this.first
     this.first = this.first.next
     if (this.first) {
       this.first.prev = null
@@ -48,13 +49,14 @@ function LinkedList () {
       this.last = null
     }
     this.size--
-    return true
+    return first
   }
 
   this.removeLast = function () {
     if (this.size === 0) {
       return false
     }
+    const last = this.last
     this.last = this.last.prev
     if (this.last) {
       this.last.next = null
@@ -62,7 +64,7 @@ function LinkedList () {
       this.first = null
     }
     this.size--
-    return true
+    return last
   }
 
   this.removeIndex = function (index) {
@@ -71,11 +73,9 @@ function LinkedList () {
     }
     switch (index) {
       case 1:
-        this.removeFirst()
-        break
+        return this.removeFirst()
       case (this.size - 1):
-        this.removeLast()
-        break
+        return this.removeLast()
       default:
         let count = 1
         let currentNode = this.first
@@ -100,10 +100,130 @@ function LinkedList () {
       arr.push(currentNode.ele)
       currentNode = currentNode.next
     }
-    return '[' + arr.join(',') + ']'
+    return arr.join(',')
   }
 }
 
-export {
-  LinkedList
+export const Tree = function () {
+  this.first = null
+  this.last = null
+  this.floor = 0
+
+  function Node (ele) {
+    this.ele = ele
+    this.lc = null
+    this.rc = null
+    this.father = null
+  }
+  // 往二叉树加入一个节点
+  this.add = function (ele) {
+    const node = new Node(ele)
+    if (this.floor === 0) { // 一个元素都没有
+      this.first = node
+      this.floor++
+    } else if (this.floor === 1) { // 只有一个元素
+      this.last.lc = node
+      node.father = this.last
+      this.floor++
+    } else {
+      let current = this.last
+      while (current) {
+        // 当前节点是父节点的左节点
+        if (current.father.lc === current) {
+          if (current.father.rc) {
+            // 父节点有右节点
+            current = current.father.rc
+            while (current.lc) {
+              current = current.lc
+            }
+            current.lc = node
+            node.father = current
+            break
+          } else {
+            // 添加一个兄弟节点
+            current.father.rc = node
+            node.father = current.father
+            break
+          }
+        // 当前节点是父节点的右节点
+        } else {
+          if (current.father === this.first) {
+            current = this.first
+            while (current.lc) {
+              current = current.lc
+            }
+            current.lc = node
+            node.father = current
+            this.floor++
+            break
+          }
+          current = current.father
+        }
+      }
+    }
+    this.last = node
+  }
+  // 删除二叉树的最后一个节点
+  this.removeLast = function () {
+  }
+  // 深度优先遍历
+  this.toStringDeepFirst = function () {
+    function next (current) {
+      const ele = current.ele
+      eleArr.push(ele)
+      // 深度优先，所以只要有左节点就继续往下找
+      if (current.lc) {
+        next(current.lc)
+        return
+      }
+      // 没有左节点，需要看是否有父节点，父节点是否有右节点
+      // (不用查看是否有右节点，因为是二叉树，没有左节点一定没有右节点)
+      // 关键：父节点的右节点不能是当前节点，否则会死循环
+      while (current && current.father) {
+        if (current.father.rc && current.father.rc !== current) {
+          next(current.father.rc)
+          return
+        }
+        current = current.father
+      }
+    }
+
+    // function next (current) {
+    //   const ele = current.ele
+    //   eleArr.push(ele)
+    //   if (current.lc) {
+    //     next(current.lc)
+    //   }
+    //   if (current.rc) {
+    //     next(current.rc)
+    //   }
+    // }
+    const eleArr = []
+    if (this.first) {
+      next(this.first)
+    }
+    return eleArr.join(',')
+  }
+  // 广度优先遍历
+  this.toStringBreadthFirst = function () {
+    const eleArr = []
+    let current = this.first
+    if (!current) {
+      return ''
+    }
+    const linkedList = new LinkedList()
+    linkedList.addLast(current)
+    while (linkedList.size > 0) {
+      // 利用队列来进行节点的遍历，先进先出，先左后右
+      const linkedFirst = linkedList.removeFirst().ele
+      eleArr.push(linkedFirst.ele)
+      if (linkedFirst.lc) {
+        linkedList.addLast(linkedFirst.lc)
+      }
+      if (linkedFirst.rc) {
+        linkedList.addLast(linkedFirst.rc)
+      }
+    }
+    return eleArr.join(',')
+  }
 }
