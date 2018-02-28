7c270cf1abdc6e0abccc687e10d391380a1bc517
wangchong <chong.wang@iqunxing.com>
2018-02-28 15:25:05 +0800

    ES6 class 继承

--- src/components/index/basic/Inherits.vue
+++ src/components/index/basic/Inherits.vue
@@ -32,6 +32,62 @@
     console.log(child)
     // child包含了father的print方法
     </pre>
+    <h3>ES6 class</h3>
+    <pre>
+    class SuperClass {
+      superPrint () {
+        console.log('superprint')
+        console.log('className=' + this.className)
+      }
+
+      static superMethod () {
+        console.log('it is static superMethod')
+      }
+    }
+
+    class SubClass extends SuperClass {
+      constructor () {
+        super()
+        this.className = 'SubClass'
+      }
+    }
+
+    new SubClass().superPrint()
+    console.log(new SubClass())
+    SuperClass.superMethod()
+    /*
+    * 打印信息如下
+    * superprint
+    * className=SubClass
+    * 子类，原型链上有父类方法，静态方法不能被继承，只能通过父类直接调用，不需要实例
+    * it is static superMethod
+    */
+    </pre>
+    <h3>实现只能被继承的类</h3>
+    <pre>
+    class Animal {
+      constructor () {
+        if (new.target === Animal) {
+          throw new Error('此类必须被继承才能使用')
+        }
+      }
+
+      shout () {
+        console.log('不知道该怎么叫...')
+      }
+    }
+
+    // new Animal()
+
+    class Dog extends Animal {
+      shout () {
+        console.log('旺旺')
+      }
+    }
+
+    new Dog().shout()
+    </pre>
+    <p>Animal类只能被继承才能实现shout方法，不能直接实例化，类似于java中的接口</p>
   </div>
 </template>
 
@@ -60,6 +116,57 @@ inherits(Child, Father)
 const child = new Child()
 console.log(child)
 
+class SuperClass {
+  superPrint () {
+    console.log('superprint')
+    console.log('className=' + this.className)
+  }
+
+  static superMethod () {
+    console.log('it is static superMethod')
+  }
+}
+
+class SubClass extends SuperClass {
+  constructor () {
+    super()
+    this.className = 'SubClass'
+  }
+}
+
+new SubClass().superPrint()
+console.log(new SubClass())
+SuperClass.superMethod()
+
+class Animal {
+  constructor (age) {
+    if (new.target === Animal) {
+      throw new Error('此类必须被继承才能使用')
+    } else {
+      this.age = age
+    }
+  }
+
+  shout () {
+    console.log('不知道该怎么叫...')
+  }
+}
+
+// new Animal()
+
+class Dog extends Animal {
+  constructor (age, weight) {
+    super(age)
+    this.weight = weight
+  }
+  shout () {
+    console.log('旺旺')
+  }
+}
+
+const dog = new Dog(2, 10)
+dog.shout()
+console.log(dog)
+
 export default {}
 </script>
-
